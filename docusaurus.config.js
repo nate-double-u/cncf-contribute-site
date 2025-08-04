64ee9101e37916df6cb957031d3547b48e3b2b9e
Jorge O. Castro <jorge.castro@gmail.com>
2025-08-04 09:47:41 -0400

    Tutorial -> Contribute
    Signed-off-by: Jorge O. Castro <jorge.castro@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Contribute',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
